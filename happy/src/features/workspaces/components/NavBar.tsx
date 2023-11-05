57a5b1cf95e2cdd6a947965cadcb5b8182c70967
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-11-05 15:41:05 -0800

    path 설정

--- happy/src/features/workspaces/components/NavBar.tsx
+++ happy/src/features/workspaces/components/NavBar.tsx
@@ -1,19 +1,37 @@
 import Breadcrumbs from '@mui/material/Breadcrumbs'
 import Link from '@mui/material/Link'
+import { useParams } from 'react-router-dom'
+import { useAppSelector } from '../../../app/hook'
+import { selectAllWorkspace } from '../workspacesSlice'
+import { selectAllCollection } from '../../collections/collectionsSlice'
 
 export default function NavBar() {
+  const {workspaceId, collectionId, requestId} = useParams()
+  
+  // 현재 workspaceId의 해당하는 workspace를 찾아서 title을 가져온다.
+  const allWorkspaces = useAppSelector(selectAllWorkspace)
+  const workspaceTitle = allWorkspaces.find((ws) => ws.id === Number(workspaceId))?.title
+ 
+  const allCollections = useAppSelector(selectAllCollection)
+  const collectionTitle = allCollections.find((c) => c.id === Number(collectionId))?.title
+
   return (
     <div role="presentation">
-      <Breadcrumbs separator=">" aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/workspaces/0">
-          My Workspace
+      <Breadcrumbs separator="/" aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href={`/workspaces/${workspaceId}`}>
+          {workspaceTitle || 'Workspace'}
         </Link>
-        <Link underline="hover" color="inherit" href="/workspaces/0/collections/0">
-          My Collection
-        </Link>
-        <Link underline="hover" color="inherit" href="/workspaces/0/collections/0/requests/0">
-          My Request
-        </Link>
+        {collectionId && (
+          <Link underline="hover" color="inherit" href={`/workspaces/${workspaceId}/collections/${collectionId}`}>
+            {collectionTitle || 'Collection'}
+          </Link>
+        )}
+        {requestId && (
+          <Link underline="hover" color="inherit" href={`/workspaces/${workspaceId}/collections/${collectionId}/requests/${requestId}`}>
+            Request
+          </Link>
+        )}
+       
       </Breadcrumbs>
     </div>
   )
