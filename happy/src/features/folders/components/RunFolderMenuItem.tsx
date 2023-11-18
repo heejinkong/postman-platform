b2bc4605b9254fcf6512db67bef880ff048e5dff
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-11-19 00:20:20 +0900

    RunFolder 수정

--- happy/src/features/folders/components/RunFolderMenuItem.tsx
+++ happy/src/features/folders/components/RunFolderMenuItem.tsx
@@ -1,10 +1,13 @@
 import { useAppDispatch, useAppSelector } from '../../../app/hook'
 import { MenuItem, Typography } from '@mui/material'
-import { selectFolderById } from '../foldersSlice'
+import { selectAllFolders, selectFolderById } from '../foldersSlice'
 import { runTestItem } from '../../runTests/runTestItem'
 import { selectAllRequests } from '../../requests/requestsSlice'
 import runTestService from '../../runTests/service/runTestService'
 import { useNavigate } from 'react-router-dom'
+import { selectCollectionById } from '../../collections/collectionsSlice'
+import { runResultItem } from '../../runResults/runResultItem'
+import runResultService from '../../runResults/service/runResultService'
 
 type runFolderMenuItemProps = {
   parentId: string
@@ -16,24 +19,41 @@ export default function RunFolderMenuItem(props: runFolderMenuItemProps) {
   const navigate = useNavigate()
 
   const folder = useAppSelector((state) => selectFolderById(state, props.parentId))
+
+  const collection = useAppSelector((state) => selectCollectionById(state, folder?.parentId ?? ''))
+
+  const folders = useAppSelector(selectAllFolders).filter((folder) => {
+    return folder.parentId === props.parentId
+  })
+
   const requests = useAppSelector(selectAllRequests).filter((request) => {
     return request.parentId === props.parentId
   })
+
   const handleRunClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     props.handleClose(e)
 
-    requests.forEach((request) => {
-      const newRunTestItem = new runTestItem()
-      newRunTestItem.title = folder.title + ' - ' + request.title
-      newRunTestItem.parentId = folder?.id ?? ''
-      newRunTestItem.requestId = request.id
-      newRunTestItem.created = Date.now()
-      newRunTestItem.status = request.response.statusCode
-      newRunTestItem.result = request.response.body
-      dispatch(runTestService.new(newRunTestItem))
+    const newRunResultItem = new runResultItem()
+    newRunResultItem.title = collection.title
+    newRunResultItem.workspaceId = folder.workspaceId
+    newRunResultItem.parentId = folder?.id ?? ''
+    newRunResultItem.created = Date.now()
+    dispatch(runResultService.new(newRunResultItem))
 
-      navigate(`/workspaces/${folder.workspaceId}/runResult/${newRunTestItem.id}`)
-    })
+    if (requests.length > 0) {
+      requests.forEach((request) => {
+        const newRunTestItem = new runTestItem()
+        newRunTestItem.title = request.title
+        newRunTestItem.parentId = folder?.id ?? ''
+        newRunTestItem.requestId = request.id
+        newRunTestItem.created = Date.now()
+        newRunTestItem.status = request.response.statusCode
+        newRunTestItem.result = request.response.body
+        dispatch(runTestService.new(newRunTestItem))
+        newRunResultItem.runTestList?.push(newRunTestItem.id)
+      })
+    }
+    navigate(`/workspaces/${folder.workspaceId}/runResult/${newRunResultItem.id}`)
   }
 
   return (
--- happy/src/features/home/HomePage.tsx
+++ happy/src/features/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@mui/material'
+import { Box, Container, Paper, Typography } from '@mui/material'
 import WorkspacesList from '../workspaces/components/WorkspacesList'
 import NewWorkspace from '../workspaces/components/NewWorkspace'
 
@@ -7,17 +7,39 @@ export default function HomePage() {
     <Container
       sx={{
         height: '100%',
-        display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
       }}
     >
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <WorkspacesList />
+      <Box
+        sx={{
+          justifyContent: 'center',
+          backgroundColor: '#FAFAFB',
+          height: '15%',
+          width: '200%',
+          ml: -70.46,
+          mt: 0
+        }}
+      >
+        <Box>
+          <Typography variant="h3" display="block" gutterBottom sx={{ ml: 70 }}>
+            Workspace List
+          </Typography>
+
+          <Typography variant="body1" display="block" gutterBottom sx={{ mt: 7, ml: 70 }}>
+            Please select the workspace you'd like to move to.
+          </Typography>
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <NewWorkspace />
+      </Box>
+
+      <Box sx={{ mt: 20 }}>
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <WorkspacesList />
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <NewWorkspace />
+          </Box>
         </Box>
       </Box>
     </Container>
