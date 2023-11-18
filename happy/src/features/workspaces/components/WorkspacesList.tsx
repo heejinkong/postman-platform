8c21bc5747dbae2a0b2f1acc965ae052a8e20341
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-11-18 11:39:12 +0900

    workspaceLsit pagination 적용

--- happy/src/features/workspaces/components/WorkspacesList.tsx
+++ happy/src/features/workspaces/components/WorkspacesList.tsx
@@ -5,17 +5,30 @@ import Avatar from '@mui/material/Avatar'
 import { useAppDispatch, useAppSelector } from '../../../app/hook'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 import { useNavigate } from 'react-router-dom'
-import { Box, IconButton, ListItemButton, Typography } from '@mui/material'
+import { Box, IconButton, ListItemButton, Pagination, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { selectAllWorkspaces } from '../workspacesSlice'
 import workspaceService from '../service/workspaceService'
 import { workspaceItem } from '../workspaceItem'
+import { useState } from 'react'
 
 export default function WorkspacesList() {
   const navigate = useNavigate()
 
   const dispatch = useAppDispatch()
   const allWorkspaces = useAppSelector(selectAllWorkspaces)
+
+  //allWorkspaces를 최대 10개씩 나눠서 pagination으로 보여줄거야
+  const rowsPerPage = 10
+  const [currentPage, setCurrentPage] = useState(1)
+  const indexOfLastRow = currentPage * rowsPerPage
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage
+  const currentRows = allWorkspaces.slice(indexOfFirstRow, indexOfLastRow)
+  const totalPages = Math.ceil(allWorkspaces.length / rowsPerPage)
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page)
+  }
 
   const handleNavWorkspace = (workspaceId: string) => {
     navigate(`/workspaces/${workspaceId}`)
@@ -40,21 +53,39 @@ export default function WorkspacesList() {
   } else {
     return (
       <Box>
-        <List sx={{ width: '100%', maxWidth: 380 }}>
-          {allWorkspaces.map((ws) => (
-            <ListItemButton key={ws.id} onClick={() => handleNavWorkspace(ws.id)}>
-              <ListItemAvatar>
-                <Avatar>
-                  <PersonOutlineIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={ws.title} secondary={new Date(ws.created).toLocaleString()} />
-              <IconButton edge="end" aria-label="delete" onClick={(e) => handleDeleteClick(e, ws)}>
-                <DeleteIcon />
-              </IconButton>
-            </ListItemButton>
-          ))}
-        </List>
+        <Box>
+          <List sx={{ width: '100%', maxWidth: 380 }}>
+            {currentRows.map((ws) => (
+              <ListItemButton key={ws.id} onClick={() => handleNavWorkspace(ws.id)}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PersonOutlineIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={ws.title}
+                  secondary={new Date(ws.created).toLocaleString()}
+                />
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={(e) => handleDeleteClick(e, ws)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemButton>
+            ))}
+          </List>
+        </Box>
+        <Box>
+          <Pagination
+            count={totalPages}
+            shape="rounded"
+            page={currentPage}
+            onChange={(e, page) => handlePageChange(e, page)}
+            sx={{ display: 'flex', justifyContent: 'center', pb: 3 }}
+          />
+        </Box>
       </Box>
     )
   }
