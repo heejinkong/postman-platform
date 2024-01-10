ac4bb1be46ce02de37f831301a815bdc87d5225a
heejinkong <113762366+heejinkong@users.noreply.github.com>
2024-01-10 14:32:35 +0900

    Workspace list 반응형으로 변경

--- happy/src/features/home/HomePage.tsx
+++ happy/src/features/home/HomePage.tsx
@@ -1,89 +1,64 @@
-import { Avatar, Box, Typography } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 import WorkspaceList from '../workspaces/components/WorkspaceList'
-import NewWorkspace from '../workspaces/components/NewWorkspace'
-import { useAppSelector } from '../../app/hook'
-import { selectAllWorkspaces } from '../workspaces/service/workspaceSlice'
 
 export default function HomePage() {
-  const allWorkspaces = useAppSelector(selectAllWorkspaces)
-
   return (
     /* HomePage에서는 workspace들을 표시 */
-    <Box>
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      {' '}
       {/* HomePage의 상단 부분  */}
       <Box
         sx={{
-          height: '160px',
+          height: 160,
+          p: 1,
           backgroundColor: '#FAFAFB'
         }}
       >
-        {/* HomePage의 상단에는 'WorkspaceList'라는 문구와 'WorkspaceList'의 설명이 표시 */}
-        <Box sx={{ display: 'flex' }}>
-          <Box sx={{ ml: 60, mt: -5 }}>
-            <Box sx={{ display: 'flex' }}>
-              <Typography
-                variant="h4"
-                display="block"
-                gutterBottom
-                sx={{ justifyItems: 'center', mt: 11, fontWeight: 'bold' }}
-              >
-                Workspace List
-              </Typography>
-              <Typography
-                variant="body1"
-                display="block"
-                gutterBottom
-                sx={{ mt: 18, ml: -29.5, mr: 50 }}
-              >
-                Please select the workspace you'd like to move to.
-              </Typography>
-            </Box>
+        <Container
+          sx={{
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          {' '}
+          {/* HomePage의 상단에는 'WorkspaceList'라는 문구와 'WorkspaceList'의 설명이 표시 */}
+          <Box>
+            <Typography
+              variant="h4"
+              display="block"
+              gutterBottom
+              sx={{ justifyItems: 'center', fontWeight: 'bold' }}
+            >
+              Workspace List
+            </Typography>
+            <Typography variant="body1" display="block" gutterBottom sx={{}}>
+              Please select the workspace you'd like to move to.
+            </Typography>
           </Box>
-
           {/* Icon 표시 */}
-          <Box sx={{ mt: -16.5 }}>
-            <Avatar
-              alt="ToolBal Icon"
-              src="/icon.jpg"
-              sx={{
-                width: '150px',
-                height: '150px',
-                mr: '-110px',
-                mt: '140px',
-                borderRadius: '12px'
-              }}
-            />
-          </Box>
-        </Box>
+          <Box
+            component="img"
+            src="/icon.jpg"
+            sx={{
+              width: 108,
+              height: 108
+            }}
+          ></Box>
+        </Container>
       </Box>
-
       {/* HomePage의 하단 부분 */}
-      <Box sx={{ mt: 20, alignItems: 'center', justifyContent: 'center' }}>
-        {/* workspace가 있을 경우, workspace 목록 표시 */}
-        {allWorkspaces.length !== 0 ? (
-          <Box sx={{ width: '100%' }}>
-            {/* 새로운 workspace를 생성할 수 있는 버튼 표시 */}
-            <Box sx={{ display: 'flex', ml: 150.5, mt: -18 }}>
-              <NewWorkspace />
-            </Box>
-
-            {/* workspace 목록 표시 */}
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 20, mr: 23 }}>
-              <WorkspaceList />
-            </Box>
-          </Box>
-        ) : (
-          // workspace가 없을 경우, workspace 생성 버튼 표시
-          <Box sx={{ width: '100%' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 33 }}>
-              <WorkspaceList />
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
-              <NewWorkspace />
-            </Box>
-          </Box>
-        )}
-      </Box>
+      <Container sx={{ flexGrow: 1 }}>
+        {/* workspace 목록 표시 */}
+        <WorkspaceList />
+      </Container>
     </Box>
   )
 }
--- happy/src/features/workspaces/components/WorkspaceList.tsx
+++ happy/src/features/workspaces/components/WorkspaceList.tsx
@@ -10,10 +10,8 @@ import {
   IconButton,
   ListItemButton,
   Pagination,
-  Paper,
   Table,
   TableBody,
-  TableContainer,
   Typography
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
@@ -21,6 +19,7 @@ import { selectAllWorkspaces } from '../service/workspaceSlice'
 import workspaceService from '../service/workspaceService'
 import { workspaceItem } from '../domain/workspaceEntity'
 import { useState } from 'react'
+import NewWorkspace from './NewWorkspace'
 
 export default function WorkspaceList() {
   const navigate = useNavigate()
@@ -53,77 +52,87 @@ export default function WorkspaceList() {
   }
 
   if (allWorkspaces.length === 0) {
-    // Workspace가 없을 경우, 아래 문구 표시
     return (
-      <Box>
-        <Typography variant="h4" gutterBottom sx={{ ml: 10 }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Typography variant="h4" textAlign="center" mb="0.5rem">
           Workspace does not exist
         </Typography>
-        <Typography variant="h6" gutterBottom>
+        <Typography variant="h6" textAlign="center" mb="2rem">
           To start your work, try using the 'New Workspace' button at the top.
         </Typography>
-      </Box>
-    )
-  } else {
-    // Workspace가 있을 경우, Workspace 목록 표시
-    return (
-      <Box>
-        <Box>
-          {/* Workspace 목록 표시 */}
-          <TableContainer component={Paper} sx={{ mt: -18, width: 900, ml: 15 }}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-              {/* 각 Workspace에 대한 정보 표시 */}
-              <TableBody>
-                {currentRows.map((ws) => (
-                  <Box>
-                    {/* workspace 목록 클릭시 해당 workspace로 이동 */}
-                    <ListItemButton
-                      key={ws.id}
-                      onClick={() => handleNavWorkspace(ws.id)}
-                      sx={{ p: 0.1 }}
-                    >
-                      {/* workspace에 대한 avatar 표시 */}
-                      <ListItemAvatar>
-                        <Avatar sx={{ ml: 2, mr: 3, p: 2 }}>
-                          <PersonOutlineIcon />
-                        </Avatar>
-                      </ListItemAvatar>
-
-                      {/* workspace title과 생성 날짜 표시 */}
-                      <ListItemText
-                        primary={ws.title}
-                        secondary={new Date(ws.created).toLocaleString()}
-                      />
-                      {/* 아이콘 삭제 버튼 클릭 시, 해당 workspace 삭제 */}
-                      <IconButton
-                        edge="end"
-                        aria-label="delete"
-                        onClick={(e) => handleDeleteClick(e, ws)}
-                        sx={{ ml: 50, mr: 2 }}
-                        href="/"
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </ListItemButton>
-                    <Divider />
-                  </Box>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-
-        {/* Workspace 목록 Pagination 표시 (리스트 10개로 정렬)*/}
-        <Box sx={{ ml: 12, mt: 1.5 }}>
-          <Pagination
-            count={totalPages}
-            shape="rounded"
-            page={currentPage}
-            onChange={(e, page) => handlePageChange(e, page)}
-            sx={{ display: 'flex', justifyContent: 'center', pb: 10 }}
-          />
+        <Box sx={{ mx: 'auto' }}>
+          <NewWorkspace />
         </Box>
       </Box>
     )
   }
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      {/* 새로운 workspace 생성 버튼 */}
+      <Box sx={{ py: 1, display: 'flex', justifyContent: 'flex-end' }}>
+        <NewWorkspace />
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        {/* Workspace 목록 표시 */}
+        <Table size="small" aria-label="a dense table">
+          <TableBody>
+            {/* 각 Workspace에 대한 정보 표시 */}
+            {currentRows.map((ws) => (
+              <Box>
+                {/* workspace 목록 클릭시 해당 workspace로 이동 */}
+                <ListItemButton
+                  key={ws.id}
+                  onClick={() => handleNavWorkspace(ws.id)}
+                  sx={{ display: 'flex' }}
+                >
+                  {/* workspace에 대한 avatar 표시 */}
+                  <ListItemAvatar>
+                    <Avatar>
+                      <PersonOutlineIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+
+                  {/* workspace title과 생성 날짜 표시 */}
+                  <ListItemText
+                    primary={ws.title}
+                    secondary={new Date(ws.created).toLocaleString()}
+                    sx={{ flexGrow: 1 }}
+                  />
+
+                  {/* 아이콘 클릭 시, 해당 workspace 삭제 */}
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={(e) => handleDeleteClick(e, ws)}
+                    href="/"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemButton>
+                <Divider />
+              </Box>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+
+      {/* Workspace 목록 Pagination 표시 (리스트 10개로 정렬)*/}
+      <Box sx={{ py: 1 }}>
+        <Pagination
+          count={totalPages}
+          shape="rounded"
+          page={currentPage}
+          onChange={(e, page) => handlePageChange(e, page)}
+          sx={{ display: 'flex', justifyContent: 'center' }}
+        />
+      </Box>
+    </Box>
+  )
 }
